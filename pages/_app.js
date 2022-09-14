d778ccaeae9bbd2ea1d6481a0a85b1af68eefc96
Matt Jared <mattjared9@gmail.com>
2022-09-14 00:11:01 -0500

    tracing origins

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,9 @@ import Layout from "./components/Layout"
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_ROUTE_ID,
-  integrations: [new BrowserTracing()],
+  integrations: [new BrowserTracing({
+    tracingOrigins: ["localhost", "https://sentry-test-sage.vercel.app/"],
+  })],
   tracesSampleRate: 1.0,
 });
 
